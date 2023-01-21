b8c76f8d4060589bac06c0690810cc7c31172571
Shane O Hanlon <ShaneOHanlon@laptop.local>
2023-01-21 15:08:10 +0000

    Initial project setup

---
+++ energy_hub/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from energy_hub.users.models import User
+from energy_hub.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
